6176a7c0bc084eab49ec5d96d13adad5e0a7e756
Kenneth Nielsen <k.nielsen81@gmail.com>
2011-09-11 01:03:28 +0200

    Rewrote half of the file save system +spec/new-save-file-system

--- fileio.py
+++ fileio.py
@@ -17,119 +17,99 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
-import os
+import os, json
+from custom_exceptions import FileError
 
 class FileIO():
-    """ This class provides the file IO functionality 
-    """
+    """ This class provides the file IO functionality """
 
-    def __init__(self, input_file, output_extension='.proofread'):
+    def __init__(self):
         """ Initiate variables and file states """
-        self.input_file = input_file
-        self.__check_input_file(self.input_file)
+        self.ppr_file = None
+        self.out_file = None
 
-        self.output_file = self.input_file + output_extension
-        self.output_exists = False
-        self.__check_output_file(self.output_file)
 
-    def __check_input_file(self, input_file):
-        """ Check if input file can be read, otherwise raise exception """
-        if not os.access(input_file, os.R_OK):
-            raise SystemExit(1)
-
-    def __check_output_file(self, output_file):
-        """ Check if the output file exists, and if it can be read from and
-        written to, otherwise throw exceptions.
-        """
-        if os.access(output_file, os.F_OK):
-            self.output_exists = True
-            if not os.access(output_file, os.W_OK):
-                raise SystemExit(1)
-            if not os.access(output_file, os.R_OK):
-                raise SystemExit(1)
+    def read(self, input_file):
+        """ Read content dependent on filetype """
+        if os.path.splitext(input_file)[1] == '.out':
+            if os.path.splitext(os.path.splitext(input_file)[0])[1] == 'ppr':
+                """ We have tried to open the out file, overwrite and try to
+                open the .ppr file """
+                content = self.__read_ppr(os.path.splitext(input_file)[0])
+        elif os.path.splitext(input_file)[1] == '.ppr':
+            content = self.__read_ppr(input_file)
         else:
-            dirname = os.path.dirname(output_file) if\
-                os.path.dirname(output_file) != '' else '.'
+            content = self.__read_new(input_file)
+        return content
+
+    def __read_ppr(self, input_file):
+        """ Read content from .ppr file """
+        self.ppr_file = input_file
+        self.out_file = input_file + '.out'
+        self.__check_ppr_and_out_file(self.ppr_file, self.out_file, True)
+
+        with open(self.input_file) as f:
+            return json.loads(f.read())
+
+    def __read_new(self, input_file):
+        """ Read content from new file """
+        self.ppr_file = input_file + '.ppr'
+        self.out_file = self.ppr_file + '.out'
+        self.__check_ppr_and_out_file(self.ppr_file, self.out_file, False)
+
+        if not os.access(input_file, os.F_OK):
+            raise FileError(input_file, 'The file does not exist.')
+        if not os.access(input_file, os.R_OK):
+            raise FileError(input_file, 'The file is not readable.')
+        
+        with open(input_file) as f:
+            diff_chunks = f.read().split('\n\n')
+
+        diff_list = [{'diff_chunk':diff, 'comment':'', 'inline':False}
+                     for diff in diff_chunks]
+
+        return {'text':diff_list, 'encoding':'utf-8', 'bookmark':None}
+
+    def __check_ppr_and_out_file(self, ppr_file, out_file, existing):
+        """ Check file permissions """
+        if existing:
+            if not os.access(ppr_file, os.F_OK):
+                raise FileError(ppr_file, 'The file does not exist.')
+            if not os.access(ppr_file, os.R_OK):
+                raise FileError(ppr_file, 'The file is not readable.')
+            if not os.access(ppr_file, os.W_OK):
+                raise FileError(ppr_file, 'The file is not writeable.')
+        else:
+            if os.access(ppr_file, os.F_OK):
+                if not os.access(ppr_file, os.R_OK):
+                    raise FileError(ppr_file, 'The file is not readable.')
+                if not os.access(ppr_file, os.W_OK):
+                    raise FileError(ppr_file, 'The file is not writeable.')
+            else:
+                dirname = os.path.dirname(ppr_file)\
+                    if os.path.dirname(ppr_file) != '' else '.'
+                if not (os.access(dirname, os.W_OK) and
+                        os.access(dirname, os.X_OK)):
+                    raise FileError(ppr_file, 'The file cannot be created.')
+
+        if os.access(out_file, os.F_OK):
+            if not os.access(self.out_file, os.W_OK):
+                raise FileError(out_file, 'The file is not writeable.')
+        else:
+            dirname = os.path.dirname(out_file)\
+                if os.path.dirname(out_file) != '' else '.'
             if not (os.access(dirname, os.W_OK) and
                     os.access(dirname, os.X_OK)):
-                raise SystemExit(1)
-
-    def read(self):
-        """ Read files """
-        
-        with open(self.input_file) as f:
-            content = str().join(f.readlines())
-
-        # Make a list of dictionaries, taking the diff chunks by splitting
-        # the file at '\n\n'
-        self.chunks = [{'diff_chunk':cont, 'comment':''} for cont in
-                       content.split('\n\n')]
-        
-        # If we are to continue old work
-        if self.output_exists:
-            with open(self.output_file) as f:
-                content = str().join(f.readlines())
-            
-            # Split the content at '\n\n'
-            self.already_done = content.split('\n\n')
-            
-            # Make a list of just the diff chunks
-            diff_chunks = [e['diff_chunk'] for e in self.chunks]
-            last_diff = ''
-            gathering_comment = ''
-            
-            for e in self.already_done:
-                # If the current chunk is a diff chunk ..
-                if e in diff_chunks:
-                    # .. not the first one ..
-                    if last_diff != '':
-                        # .. put the gathered comment in the apropriate
-                        # dictionary
-                        self.chunks[diff_chunks.index(last_diff)]['comment'] =\
-                            gathering_comment
-                    # Replace the last diff and reset the gathered comment
-                    last_diff = e
-                    gathering_comment = ''
-                else:
-                    # If the chunk was not a diff chunk, add it to the
-                    # gathering comment
-                    if gathering_comment == '':
-                        gathering_comment = e
-                    else:
-                        gathering_comment += ('\n\n' + e)
-
-            # Make sure we write the last comment even though we do not
-            # encounter another real diff chunk after that
-            if last_diff != '':
-                self.chunks[diff_chunks.index(last_diff)]['comment'] =\
-                    gathering_comment
-
-        return self.chunks
-
+                raise FileError(out_file, 'The file cannot be created.')
 
     def write(self, content):
-        """ Write all the comments to file, if there is something to write """
+        """ Write content to ppr and out file """
+        self.__write_to_ppr(self, content)
+        self.__write_to_out(self, content)
 
-        # First check whether there is actually anything to write
-        if [e['comment'] for e in content].count('') == len(content):
-            print "no comment"
-            return
-        
-        f = open(self.output_file, 'w')
-        
-        # Write the comments
-        first = True
-        for chunk in content:
-            if chunk['comment'] != '':
-                if not first:
-                    f.write('\n\n')
-                else:
-                    first = False
-                f.write(chunk['diff_chunk'])
-                f.write('\n\n')
-                f.write(chunk['comment'])
-        f.write('\n')
+    def __write_to_ppr(self, content):
+        pass
+    
+    def __write_to_out(self, content):
+        pass
 
-        f.close()
-
-        return True
