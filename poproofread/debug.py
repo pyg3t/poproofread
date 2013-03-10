21792babe85f62d1af8ace9620780785d0822469
Kenneth Nielsen <k.nielsen81@gmail.com>
2013-03-10 09:11:30 +0100

    Debug information support in gtk gui file

--- poproofread/debug.py
+++ poproofread/debug.py
@@ -23,19 +23,45 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 def level1(method):
     """ Level 1 debug messaging """
-    def doit(*args):
+    def doit(*params, **kwargs):
         """ Decorator substitution function """
-        if args[0].level >= 1:
-            print 'DEBUG:', method.__name__, list(args)[1:]
-        return method(*args)
+        return_value = method(*params, **kwargs)
+        if params[0].level >= 1:
+            output_debug_info(method, return_value, *params, **kwargs)
+        return return_value
     return doit
 
 
 def level2(method):
     """ Level 2 debug messaging """
-    def doit(*args):
+    def doit(*params, **kwargs):
         """ Decorator substitution function """
-        if args[0].level >= 2:
-            print 'DEBUG:', method.__name__, list(args)[1:]
-        return method(*args)
+        return_value = method(*params, **kwargs)
+        if params[0].level >= 2:
+            output_debug_info(method, return_value, *params, **kwargs)
+        return return_value
     return doit
+
+
+def output_debug_info(method, ret, *params, **kwargs):
+    """ Output the methods debugging information """
+    cls = str(params[0].__class__).lstrip('__main__.')
+    meth = method.__name__
+    params_list = [format_string(str(param)) for param in params[1:]]
+    kwargs_list = [format_string('{0}={1}'.format(a, str(b))) for a, b in\
+                   kwargs.items()]
+    args_string = ',\n      '.join(params_list + kwargs_list)
+    if len(args_string) > 0:
+        args_string = '\n      ' + args_string
+    print 'DEBUG: {0}.{1}({2})\n  call returned {3}'.format(cls, meth,
+        args_string, ret)
+
+
+def format_string(obj):
+    """ Limit string representation length """
+    limit = 71  # 78 - (6 space indent and one "")") for the last argument
+    obj_str = str(obj)
+    if len(obj_str) > limit:
+        return obj_str[0:limit - 8] + ' ... ' + obj_str[-3:]
+    else:
+        return obj_str
